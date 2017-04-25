67a72221b922ea90036f5e9d1ff5708a40e4a973
Dipit Malhotra <dipitmalhotra@gmail.com>
2017-04-24 20:23:40 -0500

    Added image and Description

--- play-song/src/musicplayer/audio/Demo.java
+++ play-song/src/musicplayer/audio/Demo.java
@@ -46,7 +46,6 @@ import sun.audio.AudioStream;
 
 import java.util.*;
 import java.util.List;
-//import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -54,21 +53,28 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 public class Demo {
-	
-	
-	
-	
-//	private Clip clip;
-	
-	
-	
+
 	private boolean isPlaying=false;
 	private Text nowPlayingText;
 	private Audio audio;
 	private Sound1 sound1;
 	private String name;
 	 private Slider playbackSlider;
+	 Point p = new Point(200,200);
     JFrame frame = new JFrame("");
+    JPanel box = new JPanel();
+    JPanel Hbox = new JPanel();
+
+  
+
+//    JLabel label1 = new JLabel("", SwingConstants.LEFT); 
+//    JLabel label2 = new JLabel("", JLabel.CENTER); 
+//    JLabel label3 = new JLabel("", JLabel.CENTER);
+//    JLabel label4 = new JLabel("", JLabel.CENTER);
+//    JLabel label5 = new JLabel("", JLabel.CENTER);
+//    JLabel label6 = new JLabel("", JLabel.CENTER);
+
+   
 //    AutoCompleteDecorator decorator;
     JComboBox combobox;
 
@@ -77,9 +83,10 @@ public class Demo {
     	URL url = getClass().getResource("/Users/dipit/Documents/java/play-song/resource");
 		Clip clip = AudioSystem.getClip();
 
-    	Vector<String> toppings = new Vector<>();
+    	Vector<String> song_titles = new Vector<>();
+    	Vector<String> song_path = new Vector<>();
     	
-    	File inputFile = new File("/Users/dipit/Documents/MusicPlayer/test.xml");
+    	File inputFile = new File("/Users/dipit/Documents/MusicPlayer/play-song/src/test.xml");
         DocumentBuilderFactory dbFactory 
            = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -89,40 +96,61 @@ public class Demo {
         XPathFactory xPathfactory = XPathFactory.newInstance();
         XPath xpath = xPathfactory.newXPath();        
 //        NodeList nodeList = (NodeList) 
+
         
-        
-        combobox = new JComboBox(toppings);
+        combobox = new JComboBox(song_titles);
 
         frame.setSize(400,400);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
+        
 
+
+
+//        panel.setLayout(null);
+
+
+//        statusLabel.setSize(400,200);
+//        frame.add(statusLabel);
+
+
+//		JLabel statuslabel = new JLabel("aaa");
+//	    panel.add(statuslabel);
+//	    Dimension size = statuslabel.getPreferredSize();
+//	    statuslabel.setBounds(100, 100, size.width/10, size.height/10);
+		
+		
+		
+//        frame.getContentPane().setBackground( Color.white );
         
         doc.getDocumentElement().normalize();
         System.out.println("Root element :" 
            + doc.getDocumentElement().getNodeName());
-        
-//        
-//        System.out.println("Child element :" 
-//                + doc.
-        
         NodeList nList = doc.getElementsByTagName("title");
         for (int temp = 0; temp < nList.getLength(); temp++) {
             Node nNode = nList.item(temp);
-            System.out.println("\nCurrent Element :" 
-               + nNode.getTextContent());
+//            System.out.println("\nCurrent Element :" 
+//               + nNode.getTextContent());
             
-      	  toppings.add(nNode.getTextContent());
+      	  song_titles.add(nNode.getTextContent());
       	  
-          
         }
+      	NodeList nList1 = doc.getElementsByTagName("from");
+        for (int temp1 = 0; temp1 < nList1.getLength(); temp1++) {
+            Node nNode1 = nList1.item(temp1);
+//            System.out.println("\nCurrent Element :" 
+//               + nNode1.getTextContent());
+            
+      	  song_path.add(nNode1.getTextContent());
+        }
+          
         
-//      System.out.println(toppings);        
-
         
-        
+      System.out.println(song_path);
+                
         frame.add(combobox);
+
         
         JButton b1 = new JButton();
         b1.addActionListener(new ActionListener() {
@@ -133,52 +161,71 @@ public class Demo {
 				
 		        String x = '\'' +  String.valueOf(combobox.getSelectedItem())+'\'' ;
 
-//		        System.out.println(x);
 		        
 		        NodeList nodeList;
 				try {
+					;
 					nodeList = (NodeList)xpath.compile("/playlist/song[@title="+x+"]").evaluate(doc, XPathConstants.NODESET);
-			    	Vector<String> song_vector = new Vector<>();
-//			    	song_vector.add(nodeList.item(0).getTextContent().length());
 					System.out.println(nodeList.item(0).getTextContent());
-					System.out.println(nodeList.item(0).getTextContent().split("\n")[2]);
-					
 					String songPath=nodeList.item(0).getTextContent().split("\n")[2].replaceAll(" ", "");
-					System.out.println("2");
-
-//					URI uri = new URI(songPath);
-//					System.out.println(uri);
-//
-//					
-////					URL url = URLEncoder.encode(s);
-//					System.out.println("test"+url);
+					String Album = nodeList.item(0).getTextContent().split("\n")[3].replaceAll(" ", "");
+					String Artist =  nodeList.item(0).getTextContent().split("\n")[4].replaceAll(" ", "");
+					String year = nodeList.item(0).getTextContent().split("\n")[5].replaceAll(" ", "");
 					
-			        clip.open(AudioSystem.getAudioInputStream(new File(songPath)));
+					clip.close();
+					
+					clip.open(AudioSystem.getAudioInputStream(new File(songPath)));
+					
 			        clip.start();
+			        JLabel artist = new JLabel(Artist);
+			        JLabel album = new JLabel(Album);
+			        JLabel Year = new JLabel(year);
+//			        JLabel nl1 = new JLabel("<html><br></html>");
+
+			        JLabel a1 = new JLabel("null");
+			        JLabel a2 = new JLabel("null");
+			        JLabel a3 = new JLabel("null");
+			        JLabel image= new JLabel();
+			        ImageIcon imageIcon = new ImageIcon(new ImageIcon("/Users/dipit/Desktop/closer.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+
+			        image.setIcon(imageIcon);
 			        
+			        //			        Dimension maximumSize = new Dimension((30,30));
+//					image.size
+			        a1.setText("<html>Artist:</html>");
+			        a2.setText("<html><br>Album:</html>");
+			        a3.setText("<html><br>Year::</html>");
+			        Hbox.removeAll();
+			        Hbox.setLayout(new BoxLayout(Hbox, BoxLayout.X_AXIS));
+			        Hbox.add(image);
 
-					
-//					AudioClip clip = Applet.newAudioClip(url);
-//					clip.play();
-
-					
-//			    	System.out.println(song_vector.get(1));
+			        
+			        box.removeAll();
+			        
+			        box.setLayout(new BoxLayout(box, BoxLayout.Y_AXIS));
+			        box.add(a1);
+			        box.add(artist);
+//			        box.add(nl1);
+			        
+			        box.add(a2);
+			        box.add(album);
+			        
+			        
+			        box.add(a3);
+			        box.add(Year);
+			        
+			        
+			        
+			        frame.add(box);
+			        frame.add(Hbox);
+			        frame.setVisible(true);
+			        
+			        
 
 				} catch (XPathExpressionException | LineUnavailableException | IOException | UnsupportedAudioFileException e2) {
 					// TODO Auto-generated catch block
 					e2.printStackTrace();
 				}
-				
-				
-
-//				clip.stop();
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				isPlaying=true;
 			}});
   
         frame.setSize(500,500);     
@@ -188,27 +235,9 @@ public class Demo {
 
         frame.add(b1);
         frame.setVisible(true);
+    
         
-
-   
-        
-        
-
-//
-//        for (int i = 0; i < nodeList.getLength(); i++) {
-//            Node nNode = nodeList.item(i);
-////            System.out.println("\nCurrent Element :" 
-////               + nNode.getTextContent());
-//        }
-        
-//        System.out.println(nodeList.item(0).getTextContent());
-
-        
-        
-    	
-        
-//        System.out.println(x);
-
+//      Stop Button  
         
         JButton b2 = new JButton();
         b2.addActionListener(new ActionListener() {
@@ -228,9 +257,80 @@ public class Demo {
         b2.setText("Stop");
         frame.add(b2);
               
+   
+        
+//        Loop Button
+        JButton b3 = new JButton();
+        b3.addActionListener(new ActionListener() {
+        	  public void actionPerformed(ActionEvent evt) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        		  
+        		 
+          		  }
+
+      			@Override
+      			public void actionPerformed(java.awt.event.ActionEvent e) {
+      				 for(int i=0;i<song_path.size();i++)
+           		  {
+           			  try {
+               			  clip.close();
+						clip.open(AudioSystem.getAudioInputStream(new File(song_path.get(i))));
+						System.out.println(clip.getMicrosecondLength());
+   			            clip.start();
+   	      				System.out.println(clip.getMicrosecondPosition());
+   			            Thread.sleep(clip.getMicrosecondLength()/1000);
+
+
+					} catch (LineUnavailableException | IOException | UnsupportedAudioFileException | InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+           			  
+           		  }
+      				
+//      			
+      			}});
+        
+        frame.setSize(500,500);     
+        b3.setSize(400,400);
+        b3.setVisible(true);
+        b3.setText("Repeat");
+        frame.add(b3);
+        
+
+              
         frame.setVisible(true);
         
-    }
+        JButton b4 = new JButton();
+        b4.addActionListener(new ActionListener() {
+        	  public void actionPerformed(ActionEvent evt) {
+          		  }
+
+      			@Override
+      			public void actionPerformed(java.awt.event.ActionEvent e) {		
+      				
+      				clip.stop();
+      				clip.close();
+      			}});
+             	
+        frame.setSize(500,500);     
+        b4.setSize(400,400);
+        b4.setVisible(true);
+        b4.setText("Next");
+        frame.add(b4);
+        frame.setVisible(true);
+//        label.setDefaultLocale(null);        
+
+       
+        
+        
+        }
+    
+    
+    
+    
+    
+    
+    
 
     
     
